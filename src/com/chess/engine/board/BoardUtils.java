b3d5afff80310bfb04da81d9f06423d78db5c8fc
Thomas <thomassjmarsh@gmail.com>
2019-10-23 22:04:19 +1100

    Minor changes to .gitignore and BoardUtils

--- src/com/chess/engine/board/BoardUtils.java
+++ src/com/chess/engine/board/BoardUtils.java
@@ -12,12 +12,15 @@ public class BoardUtils {
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
 
-    public static final boolean[] SECOND_ROW = null;
-    public static final boolean[] SEVENTH_ROW = null;
+    public static final boolean[] SECOND_ROW = initRow(1);
+    public static final boolean[] SEVENTH_ROW = initRow(48);
 
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
 
+    private BoardUtils() {
+        throw new RuntimeException("Your cannot instantiate me!");
+    }
 
     private static boolean[] initColumn(int columnNumber) {
         final boolean[] column = new boolean[64];
@@ -28,9 +31,15 @@ public class BoardUtils {
         return column;
     }
 
-    private BoardUtils() {
-        throw new RuntimeException("Your cannot instantiate me!");
+    private static boolean[] initRow(int rowNumber) {
+        final boolean[] row = new boolean[NUM_TILES];
+        do {
+            row[rowNumber] = true;
+            rowNumber++;
+        } while(rowNumber % NUM_TILES_PER_ROW != 0);
+        return row;
     }
+
 
     public static boolean isValidTileCoordinate(final int coordinate) {
         return coordinate >= 0 && coordinate < NUM_TILES;
