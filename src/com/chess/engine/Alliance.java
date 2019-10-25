a4e9fc187a4f1f45e9242c074b7d9d33711c1dc1
Thomas <thomassjmarsh@gmail.com>
2019-10-25 22:00:15 +1100

    Minor changes to Alliance, Board, BlackPlayer and WhitePlayer.

--- src/com/chess/engine/Alliance.java
+++ src/com/chess/engine/Alliance.java
@@ -5,6 +5,10 @@
 
 package com.chess.engine;
 
+import com.chess.engine.player.BlackPlayer;
+import com.chess.engine.player.Player;
+import com.chess.engine.player.WhitePlayer;
+
 public enum Alliance {
 
     WHITE {
@@ -22,6 +26,12 @@ public enum Alliance {
         public boolean isBlack() {
             return false;
         }
+
+        @Override
+        public Player choosePlayer(final WhitePlayer whitePlayer,
+                                   final BlackPlayer blackPlayer) {
+            return whitePlayer;
+        }
     },
     BLACK {
         @Override
@@ -38,10 +48,17 @@ public enum Alliance {
         public boolean isBlack() {
             return true;
         }
+
+        @Override
+        public Player choosePlayer(final WhitePlayer whitePlayer,
+                                   final BlackPlayer blackPlayer) {
+            return blackPlayer;
+        }
     };
 
     public abstract int getDirection();
     public abstract boolean isWhite();
     public abstract boolean isBlack();
 
+    public abstract Player choosePlayer(WhitePlayer whitePlayer, BlackPlayer blackPlayer);
 }
